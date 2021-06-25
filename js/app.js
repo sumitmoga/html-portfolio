2f0cf41c6d3a9f7a2a9fe0668df65eaa2b2f589a
Sumit Singh <contactsumit22@gmail.com>
2021-06-25 23:17:37 +0530

    first commit with basic html structure

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+   $('.slider').slick({
+      arrows: false,
+      slidesToShow: 1,
+      dots: true,
+      appendDots:'.slider-dots',
+      dotsClass:'dots'
+   });
+});
